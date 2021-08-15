e9dcf0da1d05c95dc59b510480eb840ac9c2ba1b
N Gautam Animesh <69430071+gautamanimesh@users.noreply.github.com>
2021-08-16 03:10:52 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    let myStyle_ = {
+        minHeight: "100vh",
+        margin: "40px auto"
+    }
+    return (
+        <div className="container" style={myStyle_}>
+            This is an about component!
+            <p>A basic Todos List Web App! By PsyF3RxD-</p>
+        </div>
+    )
+}
